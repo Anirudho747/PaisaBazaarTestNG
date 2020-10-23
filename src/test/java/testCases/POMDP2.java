6212be63a877d1d26126c21d4015a19acf96594b
anir2070 <anir2070@CSLH1613.global.chainsys.com>
2020-10-23 12:40:14 +0530

    Data provider in POM manner

--- src/test/java/testCases/POMDP2.java
+++ src/test/java/testCases/POMDP2.java
@@ -1,18 +1,10 @@
 package testCases;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -21,7 +13,7 @@ import pageObject.GooglePage;
 import pageObject.HomePage;
 import utilityFiles.ExcelClass;
 
-public class POMDP {
+public class POMDP2 {
 
 	public WebDriver driver;
 	public GooglePage gp;
@@ -63,39 +55,7 @@ public class POMDP {
 		 hp.displayPriceandSelect7th();
 	}
 	
-	
-	@DataProvider(name="custDetails")
-	public Object[][] dataEntry() throws IOException
-	{
-		path = "./src/test/resources/ExcelFiles/PaisaBazaarCust.xlsx";
-		
-		FileInputStream fis = new FileInputStream(path);
-		XSSFWorkbook wb = new XSSFWorkbook(fis);
-		XSSFSheet sheet = wb.getSheet("New");
-		Cell cell = null;
-		
-		int rc = sheet.getLastRowNum()-sheet.getFirstRowNum();
-		int cc = sheet.getRow(0).getLastCellNum()-sheet.getRow(0).getFirstCellNum();
-		
-		String newA[][] = new String[rc][cc];
-		
-		
-		for(int i=1;i<=rc;i++)
-		{
-			for(int j=0;j<sheet.getRow(i).getLastCellNum();j++)
-			{
-				cell = sheet.getRow(i).getCell(j);
-				cell.setCellType(CellType.STRING);
-				String data = cell.getStringCellValue();
-				System.out.println(data);
-				newA[i-1][j] = data;
-			}
-		}
-		
-		return newA;
-	}
-	
-	@Test (dataProvider = "custDetails",priority=3)
+	@Test (dataProvider = "getCellData",dataProviderClass = ExcelClass.class,priority=3)
     public void custdetailsPage (String val,String val2,String val3,String val4,String val5,String val6,String val7,String val8) {
         System.out.println("Passed Parameter Is : " + val);
         System.out.println("Passed Parameter Is : " + val2);
@@ -106,5 +66,5 @@ public class POMDP {
         System.out.println("Passed Parameter Is : " + val7);
         System.out.println("Passed Parameter Is : " + val8);
     }
-	
+
 }
